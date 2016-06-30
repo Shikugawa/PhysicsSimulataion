abccb28b82b99de3254a42a664ec10a426add8c4
Shikugawa <Shikugawa@gmail.com>
2016-06-30 20:50:26 +0900

    厳しい

--- physics.js
+++ physics.js
@@ -1,10 +1,90 @@
-var Ball = function(x, y, z){
+var Ball = function(x, y, z, vx, vy, vz){
   this.x = x;
   this.y = y;
   this.z = z;
-
-  /* ルンゲクッタ法による振り子の座標を計算する。 初期条件(最初の座標と時間t)、タイムステップdt */
+  this.vx = vx;
+  this.vy = vy;
+  this.vz = vz;
 };
+
+  var t = 0;
+  var dt = 0.001;
+  var skip = 500;
+  var g = 9.8;
+
+  Ball.prototype.fx = function(t, x, y, z, vx, vy, vz){
+    return vx;
+  };
+
+  Ball.prototype.fvx = function(t, x, y, z, vx, vy, vz){
+    var l2 = Math.pow(x,2)  + Math.pow(y,2)  + Math.pow(z,2);
+    var v2 = Math.pow(vx,2) + Math.pow(vy,2) + Math.pow(vz,2);
+    return (g*z-v2) * x / l2 ;
+  };
+
+  Ball.prototype.fy = function(t, x, y, z, vx, vy, vz){
+    return vy;
+  };
+
+  Ball.prototype.fvy = function(t, x, y, z, vx, vy, vz){
+    var l2 = Math.pow(x,2)  + Math.pow(y,2)  + Math.pow(z,2);
+    var v2 = Math.pow(vx,2) + Math.pow(vy,2) + Math.pow(vz,2);
+    return (g*z-v2) * y / l2 ;
+  };
+
+  Ball.prototype.fz = function(t, x, y, z, vx, vy, vz){
+    return vz;
+  };
+
+  Ball.prototype.fvz = function(t, x, y, z, vx, vy, vz){
+    var l2 = Math.pow(x,2)  + Math.pow(y,2)  + Math.pow(z,2);
+    var v2 = Math.pow(vx,2) + Math.pow(vy,2) + Math.pow(vz,2);
+    return -g + (g*z-v2) * z / l2 ;
+  };
+
+  Ball.prototype.RungeKutta4 = function(){
+    var k1 = new Array(6);
+    var k2 = new Array(6);
+    var k3 = new Array(6);
+    var k4 = new Array(6);
+   
+    var x = this.x - poll.x;
+    var y = this.y - poll.y;  
+    var z = this.z - poll.z;
+    var vx = this.vx;
+    var vy = this.vy;  
+    var vz = this.vz;  
+    k1[0]=dt*this.fx(t,x,y,z,vx,vy,vz);
+    k1[1]=dt*this.fvx(t,x,y,z,vx,vy,vz);
+    k1[2]=dt*this.fy(t,x,y,z,vx,vy,vz);
+    k1[3]=dt*this.fvy(t,x,y,z,vx,vy,vz);
+    k1[4]=dt*this.fz(t,x,y,z,vx,vy,vz);
+    k1[5]=dt*this.fvz(t,x,y,z,vx,vy,vz);
+    k2[0]=dt*this.fx(t+dt/2.0, x+k1[0]/2.0,y+k1[2]/2.0,z+k1[4]/2.0,vx+k1[1]/2.0,vy+k1[3]/2.0,vz+k1[5]/2.0);
+    k2[1]=dt*this.fvx(t+dt/2.0,x+k1[0]/2.0,y+k1[2]/2.0,z+k1[4]/2.0,vx+k1[1]/2.0,vy+k1[3]/2.0,vz+k1[5]/2.0);
+    k2[2]=dt*this.fy(t+dt/2.0, x+k1[0]/2.0,y+k1[2]/2.0,z+k1[4]/2.0,vx+k1[1]/2.0,vy+k1[3]/2.0,vz+k1[5]/2.0);
+    k2[3]=dt*this.fvy(t+dt/2.0,x+k1[0]/2.0,y+k1[2]/2.0,z+k1[4]/2.0,vx+k1[1]/2.0,vy+k1[3]/2.0,vz+k1[5]/2.0);
+    k2[4]=dt*this.fz(t+dt/2.0, x+k1[0]/2.0,y+k1[2]/2.0,z+k1[4]/2.0,vx+k1[1]/2.0,vy+k1[3]/2.0,vz+k1[5]/2.0);
+    k2[5]=dt*this.fvz(t+dt/2.0,x+k1[0]/2.0,y+k1[2]/2.0,z+k1[4]/2.0,vx+k1[1]/2.0,vy+k1[3]/2.0,vz+k1[5]/2.0);
+    k3[0]=dt*this.fx(t+dt/2.0, x+k2[0]/2.0,y+k2[2]/2.0,z+k2[4]/2.0,vx+k2[1]/2.0,vy+k2[3]/2.0,vz+k2[5]/2.0);
+    k3[1]=dt*this.fvx(t+dt/2.0,x+k2[0]/2.0,y+k2[2]/2.0,z+k2[4]/2.0,vx+k2[1]/2.0,vy+k2[3]/2.0,vz+k2[5]/2.0);
+    k3[2]=dt*this.fy(t+dt/2.0, x+k2[0]/2.0,y+k2[2]/2.0,z+k2[4]/2.0,vx+k2[1]/2.0,vy+k2[3]/2.0,vz+k2[5]/2.0);
+    k3[3]=dt*this.fvy(t+dt/2.0,x+k2[0]/2.0,y+k2[2]/2.0,z+k2[4]/2.0,vx+k2[1]/2.0,vy+k2[3]/2.0,vz+k2[5]/2.0);
+    k3[4]=dt*this.fz(t+dt/2.0, x+k2[0]/2.0,y+k2[2]/2.0,z+k2[4]/2.0,vx+k2[1]/2.0,vy+k2[3]/2.0,vz+k2[5]/2.0);
+    k3[5]=dt*this.fvz(t+dt/2.0,x+k2[0]/2.0,y+k2[2]/2.0,z+k2[4]/2.0,vx+k2[1]/2.0,vy+k2[3]/2.0,vz+k2[5]/2.0);
+    k4[0]=dt*this.fx(t+dt/2.0, x+k3[0],y+k3[2],z+k3[4],vx+k3[1],vy+k3[3],vz+k3[5]);
+    k4[1]=dt*this.fvx(t+dt/2.0,x+k3[0],y+k3[2],z+k3[4],vx+k3[1],vy+k3[3],vz+k3[5]);
+    k4[2]=dt*this.fy(t+dt/2.0, x+k3[0],y+k3[2],z+k3[4],vx+k3[1],vy+k3[3],vz+k3[5]);
+    k4[3]=dt*this.fvy(t+dt/2.0,x+k3[0],y+k3[2],z+k3[4],vx+k3[1],vy+k3[3],vz+k3[5]);
+    k4[4]=dt*this.fz(t+dt/2.0, x+k3[0],y+k3[2],z+k3[4],vx+k3[1],vy+k3[3],vz+k3[5]);
+    k4[5]=dt*this.fvz(t+dt/2.0,x+k3[0],y+k3[2],z+k3[4],vx+k3[1],vy+k3[3],vz+k3[5]);
+    this.x  +=  (k1[0]+2.0*k2[0]+2.0*k3[0]+k4[0])/6.0;
+    this.vx +=  (k1[1]+2.0*k2[1]+2.0*k3[1]+k4[1])/6.0;
+    this.y  +=  (k1[2]+2.0*k2[2]+2.0*k3[2]+k4[2])/6.0;
+    this.vy +=  (k1[3]+2.0*k2[3]+2.0*k3[3]+k4[3])/6.0;
+    this.z  +=  (k1[4]+2.0*k2[4]+2.0*k3[4]+k4[4])/6.0;
+    this.vz +=  (k1[5]+2.0*k2[5]+2.0*k3[5]+k4[5])/6.0;
+  };
 
 var main = function () {
   var scene = new THREE.Scene();
@@ -53,7 +133,7 @@ var main = function () {
   poll.position.set(0, 0, 100);
   scene.add(poll);
 
-  var ball_property = new Ball(0, 60, 100);
+  var ball_property = new Ball(0, 60, 100, 0, 0, 0);
   var ball = new THREE.Mesh(
     new THREE.SphereGeometry(5, 5, 5),
     new THREE.MeshLambertMaterial(au)
@@ -82,14 +162,14 @@ var main = function () {
   var line = new THREE.Line(geometry, material);
   scene.add(line);
 
-  /*
-  var t = 0;
   (function loop(){ 
+    for(var i=1; i <= skip; i++){
+      ball_property.prototype.RungeKutta4();
+    }
+    t += dt*skip;
+
     window.requestAnimationFrame(loop);
-    sphere.rotation.set(0, 0, (t++) / 100);
-    renderer.render(scene, camera);
   })();
-  */
   
 
   renderer.render(scene, camera);
